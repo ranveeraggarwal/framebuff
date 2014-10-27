e28b95aecb43f0cf8959d6b1ea5c9e8d0d14e903
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-10-27 12:22:06 +0530

    title render

--- src/avideo/VideoPage.java
+++ src/avideo/VideoPage.java
@@ -1,11 +1,27 @@
 package avideo;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import models.Chat;
+
+import org.rythmengine.Rythm;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.util.IntegerMapper;
+import org.skife.jdbi.v2.util.StringMapper;
+
+import common.Mapper;
 
 /**
  * Servlet implementation class VideoPage
@@ -13,7 +29,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/video/*")
 public class VideoPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private DBI dbi;   
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,14 +42,36 @@ public class VideoPage extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Map <String, Object> args = new HashMap <String, Object> ();
+		String requestURI = request.getRequestURI();
+		Integer videoId = Integer.parseInt(requestURI.split("/")[2]);
+		String title;
+		dbi = (DBI)request.getServletContext().getAttribute("dbi");
+		try(Handle h = dbi.open())
+		{
+			title = h.createQuery("SELECT title FROM video WHERE videoid =:videoId")
+					.bind("videoId", videoId)
+					.map(StringMapper.FIRST)
+					.first();
+		}
+		HttpSession session = request.getSession();
+		Integer userId = (Integer) session.getAttribute("userId");
+		PrintWriter out = response.getWriter();
+		if (userId == null){
+			args.put("who", "World");
+			out.println(Rythm.render("WebContent/templates/home/index.html", args));
+		}
+		else {
+			args.put("videoTitle", title);
+			out.println(Rythm.render("WebContent/templates/video/index.html", args));
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String requestURI = request.getRequestURI();
+		
 	}
 
 }
