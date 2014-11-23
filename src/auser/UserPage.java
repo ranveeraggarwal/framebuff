c130ea313ebe9296578dfe8c808451de8ebb60d7
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-11-23 19:59:05 +0530

    Added User Page

--- src/auser/UserPage.java
+++ src/auser/UserPage.java
@@ -1,4 +1,4 @@
-package avideo;
+package auser;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,23 +11,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Video;
+import models.Users;
 
 import org.rythmengine.Rythm;
 
 import common.CommonSQL;
 
 /**
- * Servlet implementation class VideoPage
+ * Servlet implementation class UserPage
  */
-@WebServlet("/video/*")
-public class VideoPage extends HttpServlet {
+@WebServlet("/user/*")
+public class UserPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public VideoPage() {
+    public UserPage() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -38,21 +38,20 @@ public class VideoPage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Map <String, Object> args = new HashMap <String, Object> ();
 		String requestURI = request.getRequestURI();
-		Integer videoId = Integer.parseInt(requestURI.split("/")[2]);
+		Integer userId = Integer.parseInt(requestURI.split("/")[2]);
 
-		Video vidObject = CommonSQL.getVideoByVideoId(videoId);
-		CommonSQL.updateVideoWithPerson(vidObject);
+		Users userObject = CommonSQL.getUserByUserId(userId);
 
 		PrintWriter out = response.getWriter();
-		args.put("videoDetails", vidObject);
-		out.println(Rythm.render("WebContent/templates/video/index.html", args));
+		args.put("userDetails", userObject);
+		out.println(Rythm.render("WebContent/templates/users/index.html", args));
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 	}
+
 }
