119d717f545edeb3dc1a6e75950d67c0ae0ad01b
nitinchandrol <ntnchandrol@gmail.com>
2014-11-24 17:08:31 +0530

    user data added

--- database/create_user_table.sql
+++ database/create_user_table.sql
@@ -34,12 +34,39 @@ create table follow
 insert into users (firstName, lastName, username, email, countryCode, language, phone) values
     ('Dheerendra', 'Rathor', 'DheerendraRathor', 'dheeru.rathor14@gmail.com', 'IN', 'English', 8879538411),
     ('Ranveer', 'Aggarwal', 'ranveeraggarwal', 'ranveer@gmail.com', 'IN', 'English', 239423024),
-    ('Nitin', 'Chandrol', 'nitin', 'ntnhacker@gmail.com', 'IN', 'English', 2938420123);
+    ('Nitin', 'Chandrol', 'nitin', 'ntnchandrol@gmail.com', 'IN', 'English', 2938420123),
+    ('Shubham', 'Jain', 'dora', 'dora@gmail.com', 'IN', 'English', 2938420123),
+    ('Suman', 'Sourabh', 'suman', 'suman@gmail.com', 'IN', 'English', 2938420123),
+    ('Rakesh', 'Ranjan', 'rakesh', 'rakesh@gmail.com', 'IN', 'English', 2938420123),
+    ('Aman', 'Gour', 'aman', 'aman@gmail.com', 'IN', 'English', 2938420123),
+    ('Dinesh', 'Kota', 'dinu', 'dinesh@gmail.com', 'IN', 'English', 2938420123),
+    ('Mahinder', 'Kota', 'mahi', 'mahi@gmail.com', 'IN', 'English', 2938420123),
+    ('Rohit', 'Kumar', 'rohit', 'rohit@gmail.com', 'IN', 'English', 2938420123),
+    ('Manohar', 'Kumar', 'mannu', 'mannu@gmail.com', 'IN', 'English', 2938420123),
+    ('Pranay', 'Dhondi', 'pranay', 'pranay@gmail.com', 'IN', 'English', 2938420123),
+    ('Akhil', 'Mududpu', 'akhil', 'akhil@gmail.com', 'IN', 'English', 2938420123),
+    ('Jassem', 'Umar', 'jaseem', 'jaseem@gmail.com', 'IN', 'English', 2938420123);
+    
     
 insert into auth (userId, password) values
     ('1', 'dheerendra'),
     ('2', 'ranveer'),
-    ('3', 'rashmi');
+    ('3', 'nitin'),
+    ('4', 'dora'),
+    ('5', 'suman'),
+    ('6', 'rakesh'),
+    ('7', 'aman'),
+    ('8', 'dinu'),
+    ('9', 'mahi'),
+    ('10', 'rohit'),
+    ('11', 'mannu'),
+    ('12', 'pranay'),
+    ('13', 'akhil'),
+    ('14', 'jaseem')
+    ;
 
 insert into follow (followee, follower) values 
-    ('1', '2'), ('1', '3'), ('2', '1'), ('2', '3'), ('3', '1'), ('3', '2');
+    ('1', '2'), ('1', '3'), ('2', '1'), ('2', '3'), ('3', '1'), ('3', '2'),
+    ('5', '4'), ('1', '5'), ('2', '4'), ('2', '13'), ('13', '1'), ('3', '12'),
+    ('1', '8'), ('1', '7'), ('2', '9'), ('2', '5'), ('3', '11'), ('3', '10')
+    ;
